7cc57cfd888c0978e6005e89250bede19e293e5c
speedXro <cosminp96vs@gmail.com>
2020-05-19 02:42:56 +0300

    First part of implementing AudioPlayer class

--- AudioPlayerLib/AudioFile.cs
+++ AudioPlayerLib/AudioFile.cs
@@ -8,6 +8,21 @@ namespace AudioPlayerLib
 {
     class AudioFile
     {
+        private readonly string _format;
+        private readonly string _path;
+        private readonly string _name;
 
+        public string Format => _format;
+        public string Path => _path;
+        public string Name => _name;
+
+        public AudioFile(string path)
+        {
+            _path = path;
+            string[] firstSplitComponents = _path.Split('.');
+            _format = firstSplitComponents[1];
+            string[] secondSplitComponents = firstSplitComponents[0].Split('\\');
+            _name = secondSplitComponents[secondSplitComponents.Length - 1];
+        }
     }
 }
--- AudioPlayerLib/AudioPlayer.cs
+++ AudioPlayerLib/AudioPlayer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NAudio.Wave;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,76 @@ namespace AudioPlayerLib
 {
     class AudioPlayer
     {
+        public enum AudioPlayerState { Playing, Paused, Stopped };
+        public enum StopCause { EofReached, UserTriggered };
 
+        private AudioPlayerState _audioPlayerState;
+        private StopCause _stopCause;
+        private DirectSoundOut _directSoundOut;
+        private WaveStream _waveStream;
+        private WaveChannel32 _waveChannel32;
+
+        public event Action PlaybackResumed;
+        public event Action PlaybackStopped;
+        public event Action PlaybackPaused;
+
+        public AudioPlayer()
+        {
+            _audioPlayerState = AudioPlayerState.Stopped;
+
+        }
+
+        public void PlaySong(AudioFile audioFile)
+        {
+            if (_audioPlayerState == AudioPlayerState.Stopped)
+            {
+                _stopCause = StopCause.EofReached;
+                _audioPlayerState = AudioPlayerState.Playing;
+                switch (audioFile.Format.ToLower())
+                {
+                    case "mp3":
+                        _waveStream = new Mp3FileReader(audioFile.Path);
+                        break;
+                    case "wav":
+                        _waveStream = new WaveFileReader(audioFile.Path);
+                        break;
+                    case "aiff":
+                        _waveStream = new AiffFileReader(audioFile.Path);
+                        break;
+                    default:
+                        _waveStream = new AudioFileReader(audioFile.Path);
+                        break;
+                }
+                _directSoundOut = new DirectSoundOut(150);
+
+                _waveChannel32 = new WaveChannel32(_waveStream, 5, 0);
+                _waveChannel32.PadWithZeroes = false;
+                _directSoundOut.Init(_waveChannel32);
+                _directSoundOut.Play();
+            }
+            else if (_audioPlayerState == AudioPlayerState.Paused)
+            {
+                _audioPlayerState = AudioPlayerState.Playing;
+                _directSoundOut.Play();
+            }
+        }
+        public void PauseSong()
+        {
+            if (_audioPlayerState == AudioPlayerState.Playing)
+            {
+                _audioPlayerState = AudioPlayerState.Paused;
+                _directSoundOut.Pause();
+                //to be continued with triggering specific pause action
+            }
+        }
+
+        public void StopSong()
+        {
+            _directSoundOut.Stop();
+            //to be continued with triggering specific pause action
+        }
+
+
+        //to be continued
     }
 }
