739cf342ab0e2864ff809572bda3243f6e835b90
Aythriel <breaktdg@gmail.com>
2020-05-24 13:46:04 +0300

    Added debug internals directive to main facade

--- AudioPlayerLib/MusicPlayer.cs
+++ AudioPlayerLib/MusicPlayer.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//makes it so when building in debug, privates from this class are visible to unit test projects
+#if DEBUG
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UnitTesting")]
+#endif
 namespace AudioPlayerLib
 {
     public class MusicPlayer
